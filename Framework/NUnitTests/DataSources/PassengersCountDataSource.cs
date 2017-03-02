66cfd6978130ec134621e58e4489b7704109952e
Aleh Baslak <oleg.baslak@gmail.com>
2017-03-02 19:50:29 +0300

    adds example of test data soucres structure

---
+++ Framework/NUnitTests/DataSources/PassengersCountDataSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NUnitTests.DataSources
+{
+    public class PassengersCountDataSource
+    {
+        public static IEnumerable<TestCaseData> PassengerCount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
