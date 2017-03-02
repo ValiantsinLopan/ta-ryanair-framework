51beefa4296c82e415a30335fbafd77743f1336a
valeria20 <sholomova96@gmail.com>
2017-03-02 19:55:10 +0300

    modified test for Testcase 6, TestCase 11

---
+++ Framework/Framework/Steps/Steps.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Framework.Webdriver;
+
+namespace Framework.Steps
+{
+    public class Steps
+    {
+        
+        public string LogInWithFacebook()
+        {
+            UI.Pages.LogInPage loginPage = new UI.Pages.LogInPage();
+            loginPage.Open();
+            loginPage.LogInViaFacebook();
+            return loginPage.GetLoggedInFacebookUserName();
+        }
+
+        public string LogInWithGoogle()
+        {
+            UI.Pages.LogInPage loginPage = new UI.Pages.LogInPage();
+            loginPage.Open();
+            loginPage.LogInViaGoogle();
+            return loginPage.GetLoggedInGoogleUserName();
+        }
+    }
+}
---
+++ Framework/NUnitTests/Tests/LogInViaFacebookAndGoogleTest.cs
@@ -0,0 +1,27 @@
+﻿using Framework.Config;
+using Framework.Webdriver;
+using NUnit.Framework;
+
+using Framework.Steps;
+namespace NUnitTests.Tests
+{
+    [TestFixture]
+    class LogInViaFacebookAndGoogleTest:BaseTest
+    {
+        private Steps steps = new Steps();
+        private const string FACEBOOK_USERNAME ="John";
+        private const string GOOGLE_USERNAME = "John";
+
+        [Test]
+        public void OneLogInViaFacebook()
+        {
+            Assert.Equals(FACEBOOK_USERNAME, steps.LogInWithFacebook);
+        }
+
+        [Test]
+        public void OneLohInViaGoogle()
+        {
+            Assert.Equals(GOOGLE_USERNAME, steps.LogInWithGoogle());
+        }
+    }
+}
