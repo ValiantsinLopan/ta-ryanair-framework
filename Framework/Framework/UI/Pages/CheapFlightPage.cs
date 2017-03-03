ee60a114f67b6c04969a05331b77b113c216d76b
KristinaKosko <Kristina_Kosko_15@tut.by>
2017-03-03 13:53:21 +0300

    Adds methods for TestCase8 (Save trip)

--- Framework/Framework/UI/Pages/CheapFlightPage.cs
+++ Framework/Framework/UI/Pages/CheapFlightPage.cs
@@ -11,9 +11,23 @@ namespace Framework.UI.Pages
     {
         private const string LowestFareXPath = "//div[@class='calendar-content']//span[(contains(text(), 'Lowest Fare'))]//ancestor::*[position()=2]//p[@class='calendar-price']";
 
+        /// <summary>
+        /// Elements for "Save Trip" TestCase8
+        /// </summary>
+        public static IWebElement ContinueBtn => DriverFactory.Driver.FindElement(By.XPath("//span[(contains(text(), 'Continue'))]"));
+        public static IWebElement SelectRaceBtn => DriverFactory.Driver.FindElement(By.XPath("//button[@id='continue']"));
+        public static IWebElement SaveTripBtn => DriverFactory.Driver.FindElement(By.XPath("//button[@class='core-btn save-btn core-btn-wrap']"));
+        public static IWebElement ChooseRaceBtn => DriverFactory.Driver.FindElement(By.XPath("//div[@class='flight-header__min-price']"));
+
+        /// <summary>
+        /// Elements for "Lowest Fare" TestCase3
+        /// </summary>
         public static List<IWebElement> CalendarPrices => DriverFactory.Driver.FindElements(By.ClassName("calendar-price")).ToList();
         public static IWebElement ViewByMonthBtn => DriverFactory.Driver.FindElement(By.XPath("//span[(contains(text(), 'View by month'))]"));
 
+        /// <summary>
+        /// Methods for TestCase3
+        /// </summary>
         public static void ClickViewByMonth()
         {
             ViewByMonthBtn.Click();
@@ -23,6 +37,29 @@ namespace Framework.UI.Pages
         {
             IWebElement dateOdElementWithLowestFare = DriverFactory.Driver.FindElement(By.XPath(LowestFareXPath));
             return PriceParser.ParsePrice(dateOdElementWithLowestFare.Text);
+        }
+
+        /// <summary>
+        /// Methods for TestCase8
+        /// </summary>
+        public static void ClickContinue()
+        {
+            ContinueBtn.Click();
+        }
+
+        public static void SelectPrice()
+        {
+            SelectRaceBtn.Click();
+        }
+
+        public static void SelectPossibleRace()
+        {
+            ChooseRaceBtn.Click();
+        }
+
+        public static void SaveTrip()
+        {
+            SaveTripBtn.Click();
         }
     }
 }
